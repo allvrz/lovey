a3fc99256068e312724961049b91b361484d54f9
allvrz <alvarezalliahjaneee@gmail.com>
2025-10-04 08:56:01 +0800

    Update game.js

--- game.js
+++ game.js
@@ -83,17 +83,17 @@ const accessories = {
 // =============================== GAME CONTENT ==================================== //
 const levelThemes = [ { body: '#483d8b', canvas: '#6a5acd' }, { body: '#433881', canvas: '#6253c0' }, { body: '#3e3277', canvas: '#5a4cb3' }, { body: '#392d6d', canvas: '#5245a6' }, { body: '#342863', canvas: '#4a3e99' }, { body: '#2f2359', canvas: '#42378c' }, { body: '#2a1e4f', canvas: '#3a307f' }, { body: '#251945', canvas: '#322972' }, { body: '#20143b', canvas: '#2a2265' }, { body: '#1b0f31', canvas: '#221b58' }, { body: '#160a27', canvas: '#1a144b' }, { body: '#11051d', canvas: '#120d3e' }];
 const levelCompleteTexts = 
-["Great start!",
-"You're bouncy!",
-"Cloud hopper!",
-"Leap of faith!",
-"Careful steps!",
-"Getting higher!",
-"What a jumper!",
-"Riding the wave!",
-"Almost there!",
-"Star catcher!",
-"One last climb!"];
+["hi love! nakoy secret",
+"you wanna know?",
+"finish the game 😛",
+"wowow motivated",
+"careful steps!",
+"you're almost there bebe!",
+"patience bebe",
+"hapit na hapit na",
+"hint:",
+"I",
+"last one love go go!"];
 /**
  * --- LEVEL DATA GUIDE ---
  * Each object in the `levels` array is a new level.
@@ -431,12 +431,13 @@ function checkBoundsAndWinCondition() {
                     sounds.bg.loop = false; 
                     sounds.win.currentTime = 0;
                     if (soundEnabled) sounds.win.play();
-            let fallText = `wiwiwi with ${falls} falls, <br> you still did well!`;
+            let fallText = `wiwiwi with ${falls} falls, <br> you did well!`;
     if (falls === 0) fallText = "What a flawless run!";
     if (falls === 1) fallText = "Wow! with only 1 fall!";
     winMessage.innerHTML =
     `<div style="font-size: 0.7em; opacity: 0.8;">${fallText}</div>
-    <div style="font-size: 0.7em; opacity: 0.8;">I'm so proud of you lovey! 💜✨</div>
+    <div style="font-size: 0.7em; opacity: 0.8;">I'm so proud of you lovey!</div>
+    <div style="font-size: 0.5em; opacity: 0.8;"><br>Secret: I love you!💜🤗</div>
      <div style="margin-top: 15px;">
     <button id="win-play-again-button">Main Menu</button></div>`;
     document.getElementById('win-play-again-button').addEventListener('click', () => {
